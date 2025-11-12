85efd8a2d825e311f182ef66be16daab17d39668
Jens Simon <jens.simon@gmx.net>
2025-11-12 13:52:47 +0100

    Kapitel 6 Request Response und 7 Views

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-
-
-
-
+// http://localhost/impressum_view
+Route::get('/impressum_view', function () {
+    $inhalt="Willkommen";
+    return view('impressum',['inhalt' => $inhalt]);
+});
 
 // GET-Routen können mit dem Browser über die Adresse getestet werden!
 // http://localhost/impressum
@@ -34,6 +32,11 @@ Route::get("/impressum", function () {
    </html>
    ";
 });
+
+
+
+
+
 
 // für nicht GET-Routen kann man in
 // vendor/laravel/framework/src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php
@@ -132,12 +135,12 @@ Route::get("helloworld",  function () {
 // Übung 3
 // http://localhost/greeting/Jens/Simon
 Route::get("/greeting/{firstName}/{lastName}", function ($firstName, $lastName) {
-    return $firstName. " " . $lastName;
+    return $firstName . " " . $lastName;
 });
 
 
 // // http://localhost/alte_url
-Route::get("alte_url",function(){
+Route::get("alte_url", function () {
 
 
 
@@ -147,19 +150,20 @@ Route::get("alte_url",function(){
 });
 
 // http://localhost/neue_seite
-Route::get("neue_seite",function(){
+Route::get("neue_seite", function () {
     return "neue Seite";
 });
+
 use  App\Http\Controllers\EventController;
 use App\Http\Controllers\TestInvokableController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 // http://localhost/event_ansehen
-Route::get( '/event_ansehen', [EventController::class, 'tueEtwas']); // customn action
- 
+Route::get('/event_ansehen', [EventController::class, 'tueEtwas']); // customn action
+
 // http://localhost/event_ansehen/2/2025
 // http://localhost/event_ansehen/5/2030
-Route::get( '/event_ansehen/{nr}/{jahr}', [EventController::class, 'zeigeEvent']);
+Route::get('/event_ansehen/{nr}/{jahr}', [EventController::class, 'zeigeEvent']);
 
 //[UserController::class, 'show']
 // $eventObject=new App\Http\Controllers\EventController();
@@ -171,7 +175,7 @@ Route::get( '/event_ansehen/{nr}/{jahr}', [EventController::class, 'zeigeEvent']
 Route::resource('articles', ArticleController::class);
 
 
-/* REST API - Routing
+    /* REST API - Routing
 1  GET|HEAD        articles .................................................... articles.index › ArticleController@index
 2  POST            articles .................................................... articles.store › ArticleController@store
 3  GET|HEAD        articles/create ........................................... articles.create › ArticleController@create
@@ -181,17 +185,148 @@ Route::resource('articles', ArticleController::class);
 7  GET|HEAD        articles/{article}/edit ....................................... articles.edit › ArticleController@edit
   */
 
-// CRUD
+    // CRUD
 
-// Create
-// Read
-// Update
-// Delete
+    // Create
+    // Read
+    // Update
+    // Delete
 
 
-// Beide gehen auf 
+    // Beide gehen auf 
 ;
 Route::resource('articles2', Article2Controller::class)->except("show")->withoutMiddleware([VerifyCsrfToken::class]);
-Route::get('article2', [Article2Controller::class,"hallo"]);
+Route::get('article2', [Article2Controller::class, "hallo"]);
 
-Route::get('invokeTest',TestInvokableController::class);
+Route::get('invokeTest', TestInvokableController::class);
+
+
+// Übung 4
+use App\Http\Controllers\TestController;
+
+Route::get("helloworld", [TestController::class, 'printMessage']);
+
+// Übung 5
+use App\Http\Controllers\BlogPostController;
+
+
+Route::resource("blogposts", BlogPostController::class);
+
+// Kapitel 6 Request und Response
+
+// curl -X POST http://localhost/test_request_route
+
+use Illuminate\Http\Request;
+
+Route::post("/test_request_route", function (Request $request) {
+    // var_dump($request->all()); // für GET , POST ,....
+    // var_dump($request->input("nachname")); // für GET , POST ,....
+
+    // var_dump($request->only("nachname")); // nur für GET
+
+    // var_dump($request->has('vname'));
+
+    // var_dump($request->query("vname","Default")); // für GET
+
+    // var_dump($request->filled("vname")); // für GET prüft auf Inhalt eines vorhandnen Feldes
+
+    // var_dump($request->json()->all());
+    //  var_dump($request->cookie());
+    //  $browser = $request->header('User-Agent');
+    //  var_dump($browser);
+    // Validierung , Regelprüfung , leeres Feld nicht , maximal 20 Buchstaben
+    // if else
+
+    // $request->validate(
+    //     [
+    //     'nachname' => 'required | max:2',
+    //     // 'email' => 'required | email',
+    //     ]
+    // );
+    // Regeln ok, code läuft weiter
+    // Regel nicht OK , dann Redirect auf sich selbst
+if (!$request->filled("vname"))
+    return redirect()->back();
+else
+    // $request->input();
+    // var_dump($_REQUEST['nachname']);
+    //var_dump($_POST); // ['vname'] ="Jens"
+    return "ok\n";
+})->withoutMiddleware([VerifyCsrfToken::class]);
+
+
+Route::get("/antwort2",function(){
+
+    $data = [
+        'name' => 'John Doe',
+        'email' => 'johndoe@example.com',
+    ];
+
+    // return response("OK",234);//->json($data);
+// 
+    return response("Hello, World!")
+    ->withHeaders([
+                'Language' => 'englisch'
+    ]);
+});
+
+// http://localhost/download
+Route::get("/download",function() {
+    
+    $filePath = storage_path('file/test.pdf');
+    return response()->download($filePath);
+});
+
+
+
+
+// http://localhost/stream
+Route::get('/stream', function () {
+    return response()->stream(function () {
+        for ($i = 1; $i <= 5; $i++) {
+            echo "Schritt {$i} abgeschlossen<br>";
+            ob_flush();
+            flush();
+            sleep(1); // simuliert Arbeit
+        }
+        echo "Fertig!";
+    }, 200, [
+        'Content-Type' => 'text/html',
+        'Cache-Control' => 'no-cache',
+    ]);
+});
+
+use Illuminate\Support\Facades\Response;
+// http://localhost/audio-stream
+Route::get('/audio-stream', function () {
+    $audioFile = storage_path('audio_streams/song.mp3'); // 6 MB
+    
+    if (!file_exists($audioFile)) {
+        abort(404);
+    }
+    
+    $fileSize = filesize($audioFile);
+    $mime = mime_content_type($audioFile);
+    
+    return Response::stream(function () use ($audioFile) {
+        $handle = fopen($audioFile, 'rb');
+        while (!feof($handle)) {
+            echo fread($handle, 8192); // 8 KB chunks
+            ob_flush();
+            flush();
+        }
+        fclose($handle);
+    }, 200, [
+        'Content-Type' => $mime,
+        'Content-Length' => $fileSize,
+        'Accept-Ranges' => 'bytes',
+        'Cache-Control' => 'public, max-age=3600',
+    ]);
+});
+
+// http://localhost/zuGoogle
+Route::get("/zuGoogle",function(){
+    // return response()->redirectTo("https://google.de");
+    return redirect("https://google.de");
+    
+});
